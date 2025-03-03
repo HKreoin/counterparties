43c3e919d31f77c6b8dbb6a5fcd093eaec74f43c
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-04 00:02:45 +0300

    add openapi description to Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+/**
+ * @OA\Info(
+ *     title="Counterparty Management API",
+ *     version="1.0.0",
+ *     description="API for managing counterparties"
+ * )
+ *
+ * @OA\Server(
+ *     url="/api/v1",
+ *     description="API Server"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
