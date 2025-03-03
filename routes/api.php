bd426bc0686b2f3ee5b974206409cc0cddee1ede
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-04 00:02:14 +0300

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\Api\v1\CounterpartyController;
 use Illuminate\Support\Facades\Route;
 
 // Public routes
-Route::post('/v1/register', [AuthController::class, 'register']);
-Route::post('/v1/login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 // Protected routes
-Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('counterparties', CounterpartyController::class)
         ->only(['index', 'store']);
     Route::post('logout', [AuthController::class, 'logout']);
