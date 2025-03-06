ec828fe54b1dfdd0e3de4432b3ca911a9083dc46
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-07 00:45:31 +0300

    add default Layout.vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import '../css/app.css';
 import {createApp, h} from 'vue'
 import {createInertiaApp, Head, Link} from '@inertiajs/inertia-vue3'
 import {ZiggyVue} from '../../vendor/tightenco/ziggy';
+import Layout from './Layouts/Layout.vue';
 
 
 const el = document.getElementById('app')
@@ -12,7 +13,9 @@ createInertiaApp({
     title: (title) => `Counterparty App ${title}`,
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`];
+        let page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout || Layout;
+        return page;
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
