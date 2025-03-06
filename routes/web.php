71c4927056705e91f9a8b8a9eac8abc4bb181e77
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-07 00:47:21 +0300

    fix web routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\CounterpartyController;
 use Illuminate\Support\Facades\Route;
 
 // Auth routes
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [LoginController::class, 'login']);
+Route::get('/', function () {
+    return Inertia\Inertia::render('Welcome', []);
+});
 
-    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-    Route::post('/register', [RegisterController::class, 'register']);
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('counterparties', CounterpartyController::class)
+        ->only(['index', 'create', 'store'])->names('counterparties');
+    Route::post('logout', [AuthController::class, 'logout'])
+        ->name('logout');
 });
