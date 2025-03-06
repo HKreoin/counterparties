d729c5cdb2094cc99b341f4122ad6945552e2529
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-07 00:47:44 +0300

    add flash messages to HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,11 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'flash' => [
+                'message' => session('message'),
+                'error' => session('error'),
+                'success' => session('success'),
+            ]
         ]);
     }
 }
