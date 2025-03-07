605012e68cc0c3f7edd2fc6db324c409fb3f1152
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-07 03:20:02 +0300

    add pagination component

---
+++ resources/js/components/PaginationLinks.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+    pagination: {
+        type: Object,
+        required: true,
+    },
+})
+
+const makeLabel = label => {
+    if (label.includes('Previous')) {
+        return '< Назад'
+    } else if (label.includes('Next')) {
+        return 'Далее >'
+    } else {
+        return label
+    }
+}
+</script>
+
+<template>
+    <nav class="relative flex justify-center">
+        <template v-for="link in pagination.links" :key="link.label">
+            <Link
+                preserve-scroll
+                :href="link.url ?? ''"
+                v-html="makeLabel(link.label)"
+                class="flex items-center justify-center px-3 py-2 text-sm rounded-lg mx-1"
+                :class="{ 'bg-white': link.active, '!text-gray-300': !link.url }"
+            />
+        </template>
+    </nav>
+</template>
