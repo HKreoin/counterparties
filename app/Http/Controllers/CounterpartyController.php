d283152c7b028595f558d7c3f1718a3d7c9125da
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-07 22:20:52 +0300

    fix CounterpartyController.php index method

--- app/Http/Controllers/CounterpartyController.php
+++ app/Http/Controllers/CounterpartyController.php
@@ -7,6 +7,7 @@ use App\DTO\Counterparty\CreateCounterpartyDTO;
 use App\Models\Counterparty;
 use App\Services\CounterpartyService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Exception;
@@ -22,12 +23,20 @@ class CounterpartyController extends Controller
      */
     public function index(Request $request)
     {
-        $counterparties = Counterparty::when($request->search, function ($query) use ($request) {
-        $query->where('inn', 'like',  '%' . $request->search . '%')
-            ->orWhere('name', 'like', '%' . $request->search . '%')
-            ->orWhere('ogrn', 'like', '%' . $request->search . '%');
-        })->paginate(5)->withQueryString();
         $searchTerm = $request->search;
+
+        $counterparties = Counterparty::where('user_id', Auth::id())
+            ->when($searchTerm, function ($query) use ($searchTerm) {
+                $query->where(function($q) use ($searchTerm) {
+                    $q->where('inn', 'like', '%' . $searchTerm . '%')
+                        ->orWhere('name', 'like', '%' . $searchTerm . '%')
+                        ->orWhere('ogrn', 'like', '%' . $searchTerm . '%')
+                        ->orWhere('address', 'like', '%' . $searchTerm . '%');
+                });
+            })
+            ->paginate(5)
+            ->withQueryString();
+
         return Inertia::render('Counterparty/Index', compact('counterparties', 'searchTerm'));
     }
 
@@ -49,8 +58,8 @@ class CounterpartyController extends Controller
         if (!auth()->check()) {
             return redirect()->route('login')->withErrors(['error', 'Нужно войти в учетную запись']);
         }
+
         try {
-            Log::info('step2');
             $this->counterpartyService->createCounterparty(auth()->user(), $data);
             return redirect()->route('counterparties.index')->with('success', 'Контрагент добавлен в базу');
         } catch (Exception $e) {
@@ -60,7 +69,7 @@ class CounterpartyController extends Controller
                 'inn' => $data->inn,
             ]);
 
-            return redirect()->route('counterparties.index')->with('error', $message);
+            return redirect()->route('counterparties.create')->with('error', $message);
         }
     }
 }
