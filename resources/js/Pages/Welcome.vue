29058960c51c3f400f108f69653d85df02d5f0ca
Ilshat Khabirov <khabdev@yandex.ru>
2025-03-07 22:39:11 +0300

    fix Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,33 +4,35 @@ import {Button} from '@/components/ui/button/index.js';
 
 <template>
     <Head title="| Welcome"/>
-    <div>
+    <div class="flex h-screen items-center justify-center">
         <div>
-            <h1 class="text-blue-500 text-4xl">Добро пожаловать</h1>
+            <div>
+                <h1 class="text-blue-500 text-4xl">Добро пожаловать</h1>
+            </div>
+            <div class="mt-4 text-center text-sm">
+                Уже есть аккаунт?
+                <a
+                    :href="route('login')"
+                    class="underline"
+                > Войти </a>
+            </div>
+            <div class="mt-4 text-center text-sm">
+                Нет аккаунта?
+                <a
+                    :href="route('register')"
+                    class="underline"
+                > Зарегистрироваться </a>
+            </div>
+            <Link
+                :href="route('logout')"
+                method="post"
+                class="nav-link w-full mt-2"
+                as="button"
+                type="button"
+            >
+                <Button>Выйти</Button>
+            </Link>
         </div>
-        <div class="mt-4 text-center text-sm">
-            Уже есть аккаунт?
-            <a
-                :href="route('login')"
-                class="underline"
-            > Войти </a>
-        </div>
-        <div class="mt-4 text-center text-sm">
-            Нет аккаунта?
-            <a
-                :href="route('register')"
-                class="underline"
-            > Зарегистрироваться </a>
-        </div>
-        <Link
-            :href="route('logout')"
-            method="post"
-            class="nav-link w-full mt-2"
-            as="button"
-            type="button"
-        >
-            <Button>Выйти</Button>
-        </Link>
     </div>
 
 </template>
